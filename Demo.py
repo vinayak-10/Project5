d743705704af026988d1e7d7c5aa0afb41b17189
vinayak-10 <vinayak628@gmail.com>
2019-04-10 16:23:10 +0530

    Demo1 added

--- Demo.py
+++ Demo.py
@@ -6,17 +6,18 @@ ext = 9,
 
 class Subject(object):
 
-    def __init__(self, **s1):
-        self.code = s1['code']
-        self.title = s1['title']
-        self.lecs = s1['lecs']
-        self.labs = s1['labs']
-        self.imp = s1['imp']
-        self.course = s1['course']
+    def __init__(self, code, title, lecs, labs, imp, course):
+        self.code = code
+        self.title = title
+        self.lecs = lecs
+        self.labs = labs
+        self.imp = imp
+        self.course = course
 
 
 class Teacher(object):
     nos = 0
+
 
     def __init__(self, *sb, **de):
         self.det = {'Name': '', 'ID': '', 'in_time': '', 'out_time': ''}
@@ -30,51 +31,71 @@ class Teacher(object):
                       'sa': [None, None, None, None, None, None, None]}
 
         Teacher.nos += 1
+        self._length = 3
 
 
-    """for k in de.keys():
-       if k in self.det.keys():
-           self.det[k] = de[k]
-       if k in self.slots.keys():
-            self.slots[k] = de[k]"""
-    # self.slots = {k:v for k,v in de.items() if k in self.det.keys()}
-    def __getitem__(self, index):
-        
+    def __len__(self):
+        return self._length
 
 
-     """
-    def u_slot(self, **sl):
-        self.slots = {k:v for k,v in s1.items() if k in self.slots.keys()}
+    def __getitem__(self, k):
+        if k < 0:
+            k += len(self)  # attempt to convert negative index
 
-    def u_det(self, **s2):
-        self.det = {k:v for k,v in s2.items() if k in self.det.keys()}
+        if not 0 <= k < len(self) or not 0 <= k < len(self.subs):
+            raise IndexError("index out of range")
 
-    def u_sub(self, *sb):
-        self.subs.extend(sb)
+        if k not in self.det.keys() or self.slots.keys():
+            raise KeyError("Key not in available keys")
 
-    def clear(self, ic):
-        if ic == 0:
-            self.det = {k:None for k in self.det.keys()}
-        
-        elif ic == 1:
-            self.slots = {k:None for k in self.slots.keys()}
+        t = type(k)
+        if t == str:
+            if k in self.det.keys():
+                return self.det[k]
 
-        elif ic == 2:
-            self.subs = []
+            elif k in self.slots.keys():
+                return self.slots[k]
 
-        elif ic == 3:
-            self.det = {k:None for k in self.det.keys()}
-            self.slots = {k:None for k in self.slots.keys()}
-            self.subs = []
-        #__nos = 0
- 
-        """
+        elif t == int:
+            return self.subs[k]
 
+    def __setitem__(self, k, val):
+        if k < 0:
+            k += len(self)  # attempt to convert negative index
 
+        if not 0 <= k < len(self) or not 0 <= k < len(self.subs):
+            raise IndexError("index out of range")
 
+        if k not in self.det.keys() or self.slots.keys():
+            raise KeyError("Key not in available keys")
+
+        t = type(k)
+        if t == str:
+            if k in self.det.keys():
+                self.det[k] = val
+
+            if k in self.slots.keys():
+                self.slots[k] = val
+
+        elif t == int:
+            self.subs[k] = val
+
+    def __contains__(self, item):
+        return item in self.subs or self.slots or self.det
+    
 
 
 class Course(object):
+
+    class Sem(object):
+
+        def __init__(self, sem, sub=None):
+            if sub is None:
+                sub = []
+            self.sub_list = sub
+            self.sem = sem
+            self.st_no = 0
+
 
     def __init__(self, name, semno):
         self.cour = {'Name': name, 'semno': semno}
@@ -83,16 +104,20 @@ class Course(object):
     def _sub(self, subs):
         self.__subs.append(subs)
 
-    def _delsubs(self, code):
+    def _delsub(self):
+        code = input("Code of Subject to be deleted")
         for i in range(len(self.__subs)):
             if self.__subs[i].code == code:
                 del self.__subs[i]
-                return True
-        return False
 
-    class Sem(object):
 
-        def __init__(self, sem, sub=[]):
-            self.sub_list = sub
-            self.sem = sem
-            self.st_no = 0
+    def _suba(self):
+        return self.__subs
+
+    sub = property(fget=_suba, fset=_sub, fdel=_delsub)
+
+
+
+
+
+
